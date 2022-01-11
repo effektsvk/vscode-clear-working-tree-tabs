375da66b627ded59e2d0bb015d2a1bfba9ee6964
Erik Slovák <erik.slovak7@gmail.com>
2022-01-12 00:05:26 +0100

    add testing code for figuring out how to differentiate between git tabs

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,28 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-clear-working-tree-tabs.clearEditors', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Clear Working Tree Tabs!')
+	let disposable = vscode.commands.registerCommand('vscode-clear-working-tree-tabs.clearEditors', async () => {
+		// const initialActiveEditor = vscode.window.activeTextEditor
+
+		// TODO: figure out how to differentiate between editors and source control tabs
+		const editors = []
+		for (let index = 0; index < 4; index++) {
+			editors.push(vscode.window.activeTextEditor)
+			await vscode.commands.executeCommand('workbench.action.nextEditor')
+		}
+		console.log(editors)
+		console.log(editors.map(editor => editor?.document.getText()))
+		// if (initialActiveEditor) {
+		// 	while (true) {
+		// 		await vscode.commands.executeCommand('workbench.action.nextEditor')
+		// 		if (vscode.window.activeTextEditor?.document.uri.scheme === 'git') {
+		// 			vscode.commands.executeCommand('workbench.action.closeActiveEditor')
+		// 		}
+		// 		if (vscode.window.activeTextEditor === initialActiveEditor) {
+		// 			break
+		// 		}
+		// 	}
+		// }
 	})
 
 	context.subscriptions.push(disposable)
